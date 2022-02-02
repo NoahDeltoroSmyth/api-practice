60df95f17946aaa2788cd5c31712be319168f1e8
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-01 22:54:53 -0800

    after a long seshion wih mikey, app.test.js finally renders a linkElement

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,17 +4,24 @@ import fetchPinballMachines from '../../services/pinball';
 
 const Home = () => {
   const [machines, setMachines] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchPinballMachines();
       setMachines(data);
       console.log('data', data);
+      setLoading(false);
     };
     fetchData();
   }, []);
 
-  return <HomeList {...{ machines }} />;
+  return (
+    <div>
+      {loading && <h1>loading</h1>}
+      {!loading && <HomeList {...{ machines }} />}
+    </div>
+  );
 };
 
 export default Home;
